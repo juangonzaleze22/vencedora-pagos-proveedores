c3c6ebc8079136c898764e0b70fa41ac38f097ad
Juan Gonzalez <juangonzaleze04@gmail.com>
2026-03-07 20:25:32 -0400

    Cambio path api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lightslategray-mosquito-805509.hostingersite.com/api'
+  apiUrl: 'https://darkslateblue-duck-326409.hostingersite.com/api'
 };
