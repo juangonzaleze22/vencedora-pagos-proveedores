d98cd5a6d48094073b72811abcb59ed8ee08e489
Juan Gonzalez <juangonzaleze04@gmail.com>
2026-03-13 21:01:14 -0400

    Actualizacion url nueva api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://wheat-mallard-734202.hostingersite.com/api'
+  apiUrl: 'https://khaki-porcupine-994433.hostingersite.com/api'
 };
